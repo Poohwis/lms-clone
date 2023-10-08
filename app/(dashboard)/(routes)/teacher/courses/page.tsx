4b12a118cc33b67fcb91bb1f2cc827ae647a0e31
Poohwis <106541330+Poohwis@users.noreply.github.com>
2023-10-08 19:07:16 +0700

    first commit

---
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -0,0 +1,29 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import { DataTable } from "./_components/DataTable";
+import { columns } from "./_components/Column";
+
+const CoursePage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return (
+    <div className="p-6">
+      <DataTable columns={columns} data={courses} />
+    </div>
+  );
+};
+
+export default CoursePage;
